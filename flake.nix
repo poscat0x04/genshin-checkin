3f24a8b9e08fc1d5df3fee4fcfebf9a69989cd60
Poscat <poscat@poscat.moe>
2022-07-29 23:32:48 +0800

    initial commit

---
+++ flake.nix
@@ -0,0 +1,94 @@
+{
+  description = "THU Daily Health Report Script";
+
+  inputs = {
+    nixpkgs.url = github:poscat0x04/nixpkgs/dev;
+    flake-utils.url = github:poscat0x04/flake-utils;
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: {
+    overlay = final: prev: {
+      genshin-py = with final; with pkgs.python3Packages; buildPythonPackage rec {
+        pname = "genshin";
+        version = "1.2.2";
+
+        src = fetchPypi {
+          inherit pname version;
+          sha256 = "sha256-PugJ1Y8757s7zRdTWy1PJqIX94y4meKqozefpVj+RY0=";
+        };
+
+        buildInputs = [
+        ];
+
+        propagatedBuildInputs = [
+          aiohttp
+          pydantic
+          yarl
+        ];
+
+        doCheck = false;
+      };
+      genshin-checkin = prev.writers.writePython3 "thu-checkin.py"
+        {
+          libraries = [ final.genshin-py ];
+          flakeIgnore = [ "E501" ];
+        }
+        (builtins.readFile ./genshin-checkin.py);
+    };
+    nixosModules.genshin-checkin = { config, lib, pkgs, ... }:
+      let
+        cfg = config.services.genshin-checkin;
+        configFile = pkgs.writeText "cookies.json" (builtins.toJSON { inherit (cfg) ltoken ltuid; });
+      in
+      {
+        options.services.genshin-checkin = with lib; with types; {
+          enable = lib.mkEnableOption "hoyolab genshin check-in service";
+
+          ltoken = mkOption {
+            type = str;
+          };
+
+          ltuid = mkOption {
+            type = str;
+          };
+        };
+        config = lib.mkIf cfg.enable {
+          systemd = {
+            services.genshin-checkin = {
+              after = [ "network-online.target" ];
+              serviceConfig = {
+                Type = "oneshot";
+                ExecStart = "${pkgs.genshin-checkin} -c ${configFile}";
+              };
+            };
+            timers.genshin-checkin = {
+              wantedBy = [ "timers.target" ];
+              timerConfig = {
+                RandomizedDelaySec = "1h";
+                OnCalendar = "*-*-* 8:00:00 CST";
+                Persistent = true;
+              };
+            };
+          };
+        };
+      };
+  } // flake-utils.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; overlays = [ self.overlay ]; };
+    in
+    {
+      packages = {
+        inherit (pkgs) genshin-py genshin-checkin;
+      };
+      devShell = with pkgs; mkShell {
+        buildInputs = [
+          mypy
+          pylint
+          (python3.withPackages (p: [
+            pkgs.genshin-py
+            p.autopep8
+          ]))
+        ];
+      };
+    });
+}
---
+++ genshin-checkin.py
@@ -0,0 +1,38 @@
+from json import loads
+from json.decoder import JSONDecodeError
+import argparse
+import asyncio
+import sys
+import genshin
+
+
+async def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", metavar="CONFIG", default="cookies.json",
+                        help="the config file location, defaults to 'cookies.json'",
+                        type=str, action="store")
+    args = parser.parse_args()
+    with open(args.config, 'r', encoding="ascii") as cfg_file:
+        try:
+            cfg = loads(cfg_file.read())
+        except JSONDecodeError as err:
+            print(f"Failed to parse config file:\n  {err}")
+            return 1
+        client = genshin.Client(cfg)
+        client.USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.53 Safari/537.36"
+        client.default_game = genshin.Game.GENSHIN
+        try:
+            result = await client.claim_daily_reward()
+        except genshin.AlreadyClaimed:
+            print("You have already checked in")
+            return 0
+        except genshin.InvalidCookies:
+            print("Failed to check in, cookies are not valid")
+            return 1
+        else:
+            print(f"Successfully check in. Got {result.amount} {result.name}")
+
+
+if __name__ == "__main__":
+    ret = asyncio.run(main())
+    sys.exit(ret)
